1f60ca3f264511c7f64ae184fcecf0abef8bf0dd
emiliantolo <emiliantolo@gmail.com>
2022-06-22 18:17:19 +0200

    p

---
+++ project_tcp/sem_simulations.py
@@ -0,0 +1,28 @@
+def main():
+    import sem
+    import os
+
+    script = 'project_tcp'
+    ns_path = os.path.join(os.path.dirname(
+        os.path.realpath(__file__)), '..', 'ns-3-dev')
+    campaign_dir = "./project_campaign"
+
+    campaign = sem.CampaignManager.new(ns_path, script, campaign_dir,
+                                       runner_type='ParallelRunner',
+                                       check_repo=False,
+                                       overwrite=False)
+
+    param_combinations = {
+        'rtscts': [True, False],
+        'hidden': [True, False],
+        'tcp': [True, False],
+        'datarate': [1, 2.5, 5, 7.5, 10, 15, 20],
+        'packetsize': [1000]
+    }
+
+    campaign.run_missing_simulations(
+        sem.list_param_combinations(param_combinations), runs=10)
+
+
+if __name__ == '__main__':
+    main()
